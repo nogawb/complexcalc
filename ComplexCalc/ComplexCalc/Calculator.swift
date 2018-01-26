2fdf896e59c772682df1b1fa710ea2d092e9ec5b
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2018-01-26 13:51:27 -0800

    all but mathops from array working

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -28,6 +28,19 @@ class Calculator {
     func add(lhs: (Int, Int), rhs: (Int, Int) )-> (Int, Int) {
         return (lhs.0 + rhs.0, lhs.1 + rhs.1)
     }
+    
+   func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var finalVal = [String: Int]()
+            for (stringL, numL) in lhs {
+                for (stringR, numR) in rhs {
+                    if (stringR == stringL) {
+                        finalVal[stringR] = numL + numR
+                    }
+                }
+            }
+        return finalVal
+    }
+    
 //subtract functions
     func subtract (lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
@@ -35,6 +48,18 @@ class Calculator {
     
     func subtract(lhs: (Int, Int), rhs: (Int, Int) )-> (Int, Int) {
         return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var finalVal = [String: Int]()
+        for (stringL, numL) in lhs {
+            for (stringR, numR) in rhs {
+                if (stringR == stringL) {
+                    finalVal[stringR] = numL - numR
+                }
+            }
+        }
+        return finalVal
     }
 //multiply functions
     func multiply (lhs: Int, rhs: Int) -> Int {
@@ -68,4 +93,9 @@ class Calculator {
         }
         return sum / args.count
     }
+//MathOp
+    func mathOp(lhs : Int, rhs : Int, op : (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+
 }
--- ComplexCalc/ComplexCalc/main.swift
+++ ComplexCalc/ComplexCalc/main.swift
@@ -21,8 +21,8 @@ assert(calc.subtract(lhs: 2, rhs: 2) == 0)
 assert(calc.multiply(lhs: 2, rhs: 2) == 4)
 assert(calc.divide(lhs: 2, rhs: 2) == 1)
 //
-//assert(calc.mathOp(lhs: 5, rhs: 5, op: { ($0 + $1) + ($0 * $1) }) == 35)
-//assert(calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20)
+assert(calc.mathOp(lhs: 5, rhs: 5, op: { ($0 + $1) + ($0 * $1) }) == 35)
+assert(calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20)
 //
 assert(calc.add([1, 2, 3, 4, 5]) == 15)
 assert(calc.multiply([1, 2, 3, 4, 5]) == 120)
@@ -48,8 +48,8 @@ assert(calc.subtract(lhs: p1, rhs: p2) == (-7, 32))
 assert(calc.add(lhs: p4, rhs: p4) == (0, 0))
 assert(calc.add(lhs: p3, rhs: p4) == (-4, 4))
 //
-//let pd1 = ["x": 5, "y": 5]
-//let pd2 = ["x": -4, "y": 4]
-//assert(calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9])
-//assert(calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1])
+let pd1 = ["x": 5, "y": 5]
+let pd2 = ["x": -4, "y": 4]
+assert(calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9])
+assert(calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1])
 //
